14d72d1452077b715707d5eb073f42c7da1c1c44
unknown <hasanbb1567@gmail.com>
2020-10-24 01:27:42 -0700

    fist init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home_page.dart';
+import 'localization_service.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: HomePage(),
+      locale: LocalizationService.locale,
+      fallbackLocale: LocalizationService.fallbackLocale,
+      translations: LocalizationService(),
+    );
+  }
+}
